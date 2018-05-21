58aeef713549bf945dd8cf6a82cc9eda69281360
sensoph1 <sensoph@gmail.com>
2018-05-21 15:53:47 -0500

    added phone validation

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,6 +3,7 @@ class Entry < ApplicationRecord
     validates :first_name, presence:true
     validates :business_name, presence:true
     validates :phone, presence:true
+    validates :phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "has the wrong format try xxx-xxx-xxxx" }
     mount_uploader :image, ImageUploader
     belongs_to :user
 end
